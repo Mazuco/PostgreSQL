346345e5e7a5685a07e1132a7403534ad76c480c
Vitor Mazuco <vitor.mazuco@gmail.com>
2025-08-28 12:00:56 -0300

    Add files via upload

--- import_gpx.sh
+++ import_gpx.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-for f in `find runkeeper_gpx -name \*.gpx `
-do
-    echo "Importing gpx file $f to gpsrun.rk_track_points PostGIS table..." #, ${f%.*}"
-    ogr2ogr -append -update  -f PostgreSQL PG:"dbname='geodb' user='postgres' password='123456'" $f -nln gpsrun.rk_track_points -sql "SELECT ele, time FROM track_points"
+# Zip path
+ZIPFILE="runkeeper-gpx.zip"
+WORKDIR="gpx_tmp"
+
+# Creates temporary directory and cleans if it already exists
+rm -rf "$WORKDIR"
+mkdir "$WORKDIR"
+
+# Extract all GPX from zip
+unzip -j "$ZIPFILE" "*.gpx" -d "$WORKDIR"
+
+# Loop through the extracted GPX files
+for f in "$WORKDIR"/*.gpx; do
+    echo "Importando $f para gpsrun.rk_track_points..."
+
+    ogr2ogr -append -update \
+        -f PostgreSQL PG:"dbname='geodb' user='postgres' password='123456'" \
+        "$f" \
+        -nln gpsrun.rk_track_points \
+        -sql "SELECT ele, time FROM track_points"
 done
 
+# (optional) remove temporaries
+rm -rf "$WORKDIR"
 
